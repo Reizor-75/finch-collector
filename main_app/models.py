7980c642dce0c65b89a9a900bd63df86061c1965
mlinares <m.linares93@gmail.com>
2024-02-29 12:01:29 -0500

    create Feeding model, view and template

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,13 @@ from django.urls import reverse
 
 # Create your models here.
 
+MEALS = (
+  ('I', 'Insects'),
+  ('C', 'Cactus'),
+  ('S', 'Seeds'),
+  ('T', 'Trash')
+)
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   diet = models.CharField(max_length=100)
@@ -15,3 +22,19 @@ class Finch(models.Model):
 
   def get_absolute_url(self):
     return reverse('finch-detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField("Feeding Date")
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+  
+  class Meta:
+    ordering = ['-date']
