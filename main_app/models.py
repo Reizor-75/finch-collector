ca210534b9de323d537fd4d53d0975eca7bededf
mlinares <m.linares93@gmail.com>
2024-02-29 13:36:38 -0500

    create fed for topday model method

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from datetime import date
 # Create your models here.
 
 MEALS = (
@@ -21,7 +21,12 @@ class Finch(models.Model):
     return self.name
 
   def get_absolute_url(self):
-    return reverse('finch-detail', kwargs={'finch_id': self.id})
+    return reverse('finch-detail', kwargs={'finch_id': self.id}) 
+    
+  def fed_for_today(self):
+    if (self.diet == 'Trash'):
+      return self.feeding_set.filter(date=date.today())
+    return self.feeding_set.filter(date=date.today(), meal=str(self.diet)[:1])
 
 class Feeding(models.Model):
   date = models.DateField("Feeding Date")
