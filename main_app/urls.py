55e6c7f58784866e987e3bb63ce79e2329e4900d
mlinares <m.linares93@gmail.com>
2024-03-04 11:18:47 -0500

    restrict views to logged in users

--- main_app/urls.py
+++ main_app/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
   path('weapons/<int:pk>/update/', views.WeaponUpdate.as_view(), name='weapon-update'),
   path('weapons/<int:pk>/delete/', views.WeaponDelete.as_view(), name='weapon-delete'),
   path('finches/<int:finch_id>/assoc-weapon/<int:weapon_id>/', views.assoc_weapon, name='assoc-weapon'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,10 @@ from .models import Finch, Weapon
 from .forms import FeedingForm
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 # class Finch:  # Note that parens are optional if not inheriting from another class
@@ -27,10 +31,12 @@ class Home(LoginView):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def finch_index(request):
-  finches = Finch.objects.all()
+  finches = Finch.objects.filter(user=request.user)
   return render(request, 'finches/index.html', {'finches': finches})
 
+@login_required
 def finch_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   weapons_finch_doesnt_have = Weapon.objects.exclude(id__in = finch.weapons.all().values_list('id'))
@@ -41,6 +47,7 @@ def finch_detail(request, finch_id):
     'weapons': weapons_finch_doesnt_have
   })
 
+@login_required
 def add_feeding(request, finch_id):
   form = FeedingForm(request.POST)
   if form.is_valid():
@@ -49,36 +56,54 @@ def add_feeding(request, finch_id):
     new_feeding.save()
   return redirect('finch-detail', finch_id=finch_id)
 
+@login_required
 def assoc_weapon(request, finch_id, weapon_id):
   Finch.objects.get(id=finch_id).weapons.add(weapon_id)
   return redirect('finch-detail', finch_id=finch_id)
 
-class FinchCreate(CreateView):
+class FinchCreate(LoginRequiredMixin, CreateView):
   model = Finch
   fields = ['name', 'diet', 'beak', 'habitat', 'description']
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
 
-class FinchUpdate(UpdateView):
+class FinchUpdate(LoginRequiredMixin, UpdateView):
   model = Finch
   fields = ['habitat', 'description']
 
-class FinchDelete(DeleteView):
+class FinchDelete(LoginRequiredMixin, DeleteView):
   model = Finch
   success_url = '/finches/'
 
-class WeaponCreate(CreateView):
+class WeaponCreate(LoginRequiredMixin, CreateView):
   model = Weapon
   fields = '__all__'
 
-class WeaponList(ListView):
+class WeaponList(LoginRequiredMixin, ListView):
   model = Weapon
 
-class WeaponDetail(DetailView):
+class WeaponDetail(LoginRequiredMixin, DetailView):
   model = Weapon
 
-class WeaponUpdate(UpdateView):
+class WeaponUpdate(LoginRequiredMixin, UpdateView):
   model = Weapon
   fields = ['name', 'color']
 
-class WeaponDelete(DeleteView):
+class WeaponDelete(LoginRequiredMixin, DeleteView):
   model = Weapon
   success_url = '/weapons/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('finch-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
